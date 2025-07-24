c1562e9f1b7b252f38d6d511b9d590280f96acb1
Luca Bertoncini <lucabertoncini@inwind.it>
2025-07-24 22:40:09 +0200

    added lb_logger.py

--- src/LBWebPyBridge/zipper.sh
+++ src/LBWebPyBridge/zipper.sh
@@ -3,5 +3,5 @@ SCRIPT_FOLDER="$1"
 EXE_FOLDER="$2"
 
 cd "$SCRIPT_FOLDER"
-zip -r "$EXE_FOLDER"/python_runtime.zip worker.py LBBridge/ tests/
+zip -r "$EXE_FOLDER"/python_runtime.zip worker.py lb_logger.py LBBridge/ tests/
 

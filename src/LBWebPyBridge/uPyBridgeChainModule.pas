f340c88171205b749497136522526c6fbf634cfb
Luca <lucabertoncini@inwind.it>
2025-08-16 16:49:18 +0200

    Add files via upload

--- src/LBWebPyBridge/uPyBridgeChainModule.pas
+++ src/LBWebPyBridge/uPyBridgeChainModule.pas
@@ -5,7 +5,7 @@ unit uPyBridgeChainModule;
 interface
 
 uses
-  Classes, SysUtils, uLBmicroWebServer, uPyBridge;
+  Classes, SysUtils, uLBmicroWebServer, uPyBridge, uHTTPRequestParser;
 
 type
   { TPyBridgeChainModule }
@@ -18,17 +18,14 @@ type
 
     strict protected
       function DoProcessGETRequest(
-        var Resource: String;
-        Headers: TStringList;
-        URIParams: TStringList;
+        HTTPParser: THTTPRequestParser;
         ResponseHeaders: TStringList;
         var ResponseData: TMemoryStream;
         out ResponseCode: Integer
       ): Boolean; override;
 
       function DoProcessPOSTRequest(
-        var Resource: String;
-        Headers: TStringList;
+        HTTPParser: THTTPRequestParser;
         Payload: TMemoryStream;
         ResponseHeaders: TStringList;
         var ResponseData: TMemoryStream;
@@ -52,7 +49,7 @@ const
 implementation
 
 uses
-  ULBLogger, fpjson;
+  ULBLogger, fpjson, uHTTPConsts;
 
 { TPyBridgeChainModule }
 
@@ -69,15 +66,14 @@ begin
   inherited Destroy;
 end;
 
-function TPyBridgeChainModule.DoProcessGETRequest(var Resource: String;
-  Headers: TStringList; URIParams: TStringList; ResponseHeaders: TStringList;
-  var ResponseData: TMemoryStream; out ResponseCode: Integer): Boolean;
+function TPyBridgeChainModule.DoProcessGETRequest(HTTPParser: THTTPRequestParser;
+  ResponseHeaders: TStringList; var ResponseData: TMemoryStream; out ResponseCode: Integer): Boolean;
 begin
   Result := false;
 end;
 
-function TPyBridgeChainModule.DoProcessPOSTRequest(var Resource: String;
-  Headers: TStringList; Payload: TMemoryStream; ResponseHeaders: TStringList;
+function TPyBridgeChainModule.DoProcessPOSTRequest(HTTPParser: THTTPRequestParser;
+  Payload: TMemoryStream; ResponseHeaders: TStringList;
   var ResponseData: TMemoryStream; out ResponseCode: Integer): Boolean;
 var
   _Request : TRequestData;
@@ -85,67 +81,78 @@ var
   _sErrMsg : String;
 
 begin
-  LBLogger.Write(5, 'TPyBridgeChainModule.ProcessPOSTRequest', lmt_Debug, 'Resource <%s>', [Resource]);
+  Result := False;
+  ResponseCode := HTTP_STATUS_INTERNAL_ERROR;
 
-  // Called by THTTPRequestManager
-  // Used to insert request into orchestrator and waiting for answer
-  _Request.initialize();
-  if Resource[1] = '/' then
-    _Request.Script := Copy(Resource, 2, Length(Resource) - 1)
-  else
-    _Request.Script := Resource;
+  HTTPParser.SplitURIIntoResourceAndParameters();
 
-  if (Payload <> nil) and (Payload.Size > 0) then
+  LBLogger.Write(5, 'TPyBridgeChainModule.ProcessPOSTRequest', lmt_Debug, 'Resource <%s>', [HTTPParser.Resource]);
+
+  if HTTPParser.Resource <> '' then
   begin
-    _Request.Params := Payload.Memory;
-    _Request.ParamsLen := Payload.Size;
-  end;
+    // Called by THTTPRequestManager
+    // Used to insert request into orchestrator and waiting for answer
+    _Request.initialize();
+    if HTTPParser.Resource[1] = '/' then
+      _Request.Script := Copy(HTTPParser.Resource, 2, Length(HTTPParser.Resource) - 1)
+    else
+      _Request.Script := HTTPParser.Resource;
 
-  if FOrchestrator.insertRequest(@_Request) then
-    RTLEventWaitFor(_Request.TerminateEvent)
-  else
-    LBLogger.Write(1, 'TPyBridgeChainModule.ProcessPOSTRequest', lmt_Warning, 'Request <%s> not accepted', [Resource]);
-
-
-  if not _Request.Aborted then
-  begin
-    if (_Request.Payload <> nil) and (_Request.PayloadLen > 0) then
+    if (Payload <> nil) and (Payload.Size > 0) then
     begin
+      _Request.Params := Payload.Memory;
+      _Request.ParamsLen := Payload.Size;
+    end;
+
+    if FOrchestrator.insertRequest(@_Request) then
+      RTLEventWaitFor(_Request.TerminateEvent)
+    else
+      LBLogger.Write(1, 'TPyBridgeChainModule.ProcessPOSTRequest', lmt_Warning, 'Request <%s> not accepted', [HTTPParser.Resource]);
+
+
+    if not _Request.Aborted then
+    begin
+      if (_Request.Payload <> nil) and (_Request.PayloadLen > 0) then
+      begin
+        if ResponseData = nil then
+          ResponseData := TMemoryStream.Create
+        else
+          ResponseData.Clear;
+
+        ResponseData.Write(_Request.Payload^, _Request.PayloadLen);
+      end;
+
+    end
+    else begin
+      _ErrMsg := TJSONObject.Create();
+
+      if (_Request.Payload <> nil) and (_Request.PayloadLen > 0) then
+      begin
+        SetLength(_sErrMsg, _Request.PayloadLen);
+        Move(_Request.Payload^, _sErrMsg[1], _Request.PayloadLen);
+        _ErrMsg.Add(cInvalidAnswer, _sErrMsg);
+      end
+      else
+        _ErrMsg.Add(cInvalidAnswer, cOperationAborted);
+
+      _ErrMsg.CompressedJSON := True;
+      _sErrMsg := _ErrMsg.AsJSON;
+      _ErrMsg.Free;
+
       if ResponseData = nil then
         ResponseData := TMemoryStream.Create
       else
         ResponseData.Clear;
 
-      ResponseData.Write(_Request.Payload^, _Request.PayloadLen);
+      ResponseData.Write(_sErrMsg[1], Length(_sErrMsg));
     end;
 
+    ResponseCode := HTTP_STATUS_OK;
+    Result := True;
+
   end
-  else begin
-    _ErrMsg := TJSONObject.Create();
-
-    if (_Request.Payload <> nil) and (_Request.PayloadLen > 0) then
-    begin
-      SetLength(_sErrMsg, _Request.PayloadLen);
-      Move(_Request.Payload^, _sErrMsg[1], _Request.PayloadLen);
-      _ErrMsg.Add(cInvalidAnswer, _sErrMsg);
-    end
-    else
-      _ErrMsg.Add(cInvalidAnswer, cOperationAborted);
-
-    _ErrMsg.CompressedJSON := True;
-    _sErrMsg := _ErrMsg.AsJSON;
-    _ErrMsg.Free;
-
-    if ResponseData = nil then
-      ResponseData := TMemoryStream.Create
-    else
-      ResponseData.Clear;
-
-    ResponseData.Write(_sErrMsg[1], Length(_sErrMsg));
-  end;
-
-  ResponseCode := 200;
-  Result := True;
+  else
+    LBLogger.Write(1, 'TPyBridgeChainModule.DoProcessPOSTRequest', lmt_Warning, 'Resource not found!');
 end;
 
 procedure TPyBridgeChainModule.setOrchestratorParams(aThreadPoolSize: Integer; aSharedMemorySize: Integer; aWorkerTimeoutMs: Integer; const ScriptsFolder: String);
